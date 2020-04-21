be6ab43d7ed7dfb6c59b61ea4534711380c8e9b6
てつぎ <38849987+tetsugi@users.noreply.github.com>
2020-04-21 17:51:45 +0900

    Add demo (#3)
    * remove react-force-graph
    
    * Add demo

--- src/components/molecules/MarkdownUnit.tsx
+++ src/components/molecules/MarkdownUnit.tsx
@@ -1,15 +1,51 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "@emotion/styled"
 import MarkdownInput from "@/components/atoms/MarkdownInput"
-import MarkdownPreviewer from "@/components/atoms/MarkdownPreviewer"
+import HtmlPreviewer from "@/components/atoms/HtmlPreviewer"
+import markdown from "@/utils/markdown"
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-height: 160px;
 
-const MarkdownUnit: React.FC = () => (
-  <Container>
-    <MarkdownInput />
-    <MarkdownPreviewer />
-  </Container>
-)
+  > * {
+    flex: 1;
+    margin: 4px;
+  }
+
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+    max-height: initial;
+  }
+`
+
+type MarkdownUnitProps = Partial<{
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onRender: (html: string) => void
+}>
+
+const MarkdownUnit: React.FC<MarkdownUnitProps> = ({
+  value = "",
+  onChange = () => {},
+  onRender = () => {},
+}) => {
+  const __html = markdown.render(value)
+  useEffect(() => onRender(__html))
+
+  return (
+    <Container>
+      <MarkdownInput
+        value={value}
+        onChange={onChange}
+      />
+  
+      <HtmlPreviewer
+        dangerouslySetInnerHTML={{ __html }}
+      />
+    </Container>
+  )
+}
 
 export default MarkdownUnit
